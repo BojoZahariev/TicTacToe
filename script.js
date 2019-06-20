de2242dc30d662ce682097e46085f1b0e517c107
Bojo <bozhidar_zahariev@hotmail.com>
2019-06-20 16:25:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const gameBoard = (() => {
+    var boardArray = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'];
 
+    const div = (a, b) => a / b;
+    return {
+        boardArray
+    };
+})();
+console.log(gameBoard.boardArray);
