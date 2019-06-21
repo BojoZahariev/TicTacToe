55bc9166a007b99b3c263d6f8b8445e3ffa4008a
Bojo <bozhidar_zahariev@hotmail.com>
2019-06-21 15:22:03 +0100

    getting there

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 const gameBoard = (() => {
-    var boardArray = ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'];
+	var boardArray = [ 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x' ];
 
-    const div = (a, b) => a / b;
-    return {
-        boardArray
-    };
+	return {
+		boardArray
+	};
 })();
 console.log(gameBoard.boardArray);
+
+var myArray = [];
+
+var player1 = 'active';
+var player2 = 'not active';
+var cellValue = 0;
+for (let i = 0; i < 9; i++) {
+	var cells = document.getElementsByClassName('cell');
+	cells[i].style.backgroundColor = 'white';
+	cells[i].id += i + 1;
+
+	cells[i].addEventListener('click', () => {
+		if (cells[i].style.backgroundColor === 'white' && player1 === 'active') {
+			cells[i].style.backgroundColor = 'red';
+
+			player1 = 'not active';
+			player2 = 'active';
+			console.log(cells[i].id);
+		} else {
+			if (cells[i].style.backgroundColor === 'white' && player2 === 'active') {
+				cells[i].style.backgroundColor = 'blue';
+
+				player1 = 'active';
+				player2 = 'not active';
+				console.log(cells[i].id);
+			}
+		}
+	});
+}
