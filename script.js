c469707cfa5a5fd80cbebbec87326e020889d756
Bojo <bozhidar_zahariev@hotmail.com>
2019-06-22 09:51:32 +0100

    improve win conditions

--- script.js
+++ script.js
@@ -5,12 +5,9 @@ const gameBoard = (() => {
 		boardArray
 	};
 })();
-console.log(gameBoard.boardArray);
 
 var playerXmoves = [];
 var playerOmoves = [];
-var cellValue1 = 0;
-var cellValue2 = 0;
 
 var player1 = 'active';
 var player2 = 'not active';
@@ -27,13 +24,9 @@ for (let i = 0; i < 9; i++) {
 			player1 = 'not active';
 			player2 = 'active';
 			playerXmoves.push(Number(cells[i].id));
-			//cellValue1 = playerXmoves.reduce((a, b) => a + b, 0);
 			checkScore(playerXmoves, 'X');
 
 			console.log(playerXmoves);
-			console.log(cells[i].id);
-			console.log('score1 is ' + cellValue1);
-			console.log('length1 is ' + playerXmoves.length);
 		} else {
 			if (cells[i].style.backgroundColor === 'white' && player2 === 'active') {
 				cells[i].style.backgroundColor = 'blue';
@@ -41,13 +34,9 @@ for (let i = 0; i < 9; i++) {
 				player1 = 'active';
 				player2 = 'not active';
 				playerOmoves.push(Number(cells[i].id));
-				//cellValue2 = playerOmoves.reduce((a, b) => a + b, 0);
 				checkScore(playerOmoves, 'O');
 
-				console.log(cells[i].id);
 				console.log(playerOmoves);
-				console.log('score2 is ' + cellValue2);
-				console.log('length2 is ' + playerOmoves.length);
 			}
 		}
 	});
@@ -56,24 +45,19 @@ for (let i = 0; i < 9; i++) {
 var checkScore = (moves, player) => {
 	//starts checking on the third move
 	if (moves.length >= 3) {
-		if (moves.indexOf(1) !== -1 && moves.indexOf(2) !== -1 && moves.indexOf(3) !== -1) {
-			alert(player + 'Wins123');
-		} else if (moves.indexOf(4) !== -1 && moves.indexOf(5) !== -1 && moves.indexOf(6) !== -1) {
-			alert(player + 'Wins456');
-		} else if (moves.indexOf(7) !== -1 && moves.indexOf(8) !== -1 && moves.indexOf(9) !== -1) {
-			alert(player + 'Wins789');
-		} else if (moves.indexOf(1) !== -1 && moves.indexOf(4) !== -1 && moves.indexOf(7) !== -1) {
-			alert(player + 'Wins147');
-		} else if (moves.indexOf(2) !== -1 && moves.indexOf(5) !== -1 && moves.indexOf(8) !== -1) {
-			alert(player + 'Wins258');
-		} else if (moves.indexOf(3) !== -1 && moves.indexOf(6) !== -1 && moves.indexOf(9) !== -1) {
-			alert(player + 'Wins369');
-		} else if (moves.indexOf(4) !== -1 && moves.indexOf(5) !== -1 && moves.indexOf(6) !== -1) {
-			alert(player + 'Wins456');
-		} else if (moves.indexOf(1) !== -1 && moves.indexOf(5) !== -1 && moves.indexOf(9) !== -1) {
-			alert(player + 'Wins159');
-		} else if (moves.indexOf(3) !== -1 && moves.indexOf(5) !== -1 && moves.indexOf(7) !== -1) {
-			alert(player + 'Wins357');
+		//checking all of the wining patterns
+		if (
+			(moves.indexOf(1) !== -1 && moves.indexOf(2) !== -1 && moves.indexOf(3) !== -1) ||
+			(moves.indexOf(4) !== -1 && moves.indexOf(5) !== -1 && moves.indexOf(6) !== -1) ||
+			(moves.indexOf(7) !== -1 && moves.indexOf(8) !== -1 && moves.indexOf(9) !== -1) ||
+			(moves.indexOf(1) !== -1 && moves.indexOf(4) !== -1 && moves.indexOf(7) !== -1) ||
+			(moves.indexOf(2) !== -1 && moves.indexOf(5) !== -1 && moves.indexOf(8) !== -1) ||
+			(moves.indexOf(3) !== -1 && moves.indexOf(6) !== -1 && moves.indexOf(9) !== -1) ||
+			(moves.indexOf(4) !== -1 && moves.indexOf(5) !== -1 && moves.indexOf(6) !== -1) ||
+			(moves.indexOf(1) !== -1 && moves.indexOf(5) !== -1 && moves.indexOf(9) !== -1) ||
+			(moves.indexOf(3) !== -1 && moves.indexOf(5) !== -1 && moves.indexOf(7) !== -1)
+		) {
+			alert(player + ' Wins');
 		}
 	}
 };
